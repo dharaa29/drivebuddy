45b04491f5e89403a794023e9b228901fbf684ca
DHARAA\Dell <dharaa@feenix.technology>
2025-12-31 11:04:08 +0530

    app

--- app/main.py
+++ app/main.py
@@ -3,12 +3,15 @@ from app.routers.telematics_router import router as telematics_router
 from app.routers.user_router import router as user_router
 from app.routers.category_router import router as category_router
 from app.routers.blog_router import router as blog_router
-from app.routers.tag_router import router as tag_router  # ✅ ADD THIS
+from app.routers.tag_router import router as tag_router
 
 app = FastAPI()
+@app.get("/")
+def root():
+    return {"message": "Welcome DriveBuddy"}
 
 app.include_router(telematics_router)
 app.include_router(user_router)
 app.include_router(category_router)
 app.include_router(blog_router)
-app.include_router(tag_router)  # ✅ ADD THIS
+app.include_router(tag_router)
